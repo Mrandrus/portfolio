36fdc727c5b1972ec23bc58044ca23ad45c656f6
matt andrus <mattrandrus@gmail.com>
2023-07-20 23:32:45 -0600

    responsive updates to experience component

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -7,33 +7,37 @@ export default function Experience() {
       {/* {setUp} */}
       <div className="csuLocation-card-container">
         <div className="csuLocation-card">
-          <div className="csuLocation-card-upperContent">
-            <div className="csuLocation-card-upperContent-one">
-              <div className="csuLocation-card-upperContent-three-inner-container-one">
-                <h4>Site</h4>
+          <div className="csuLocaton-card-border">
+            <div className="csuLocation-card-upperContent">
+              <div className="csuLocation-card-upperContent-one">
+                <div className="csuLocation-card-upperContent-three-inner-container-one">
+                  <h4>Site</h4>
+                </div>
+              </div>
+              <div className="csuLocation-card-upperContent-two">
+                <h3>Colorado State University</h3>
+              </div>
+              <div className="csuLocation-card-upperContent-three">
+                <div className="csuLocation-card-upperContent-three-inner-container-three">
+                  <h4> 1 </h4>
+                </div>
               </div>
             </div>
-            <div className="csuLocation-card-upperContent-two">
-              <h3>Colorado State University</h3>
-            </div>
-            <div className="csuLocation-card-upperContent-three">
-              <div className="csuLocation-card-upperContent-three-inner-container-three">
-                <h4> 1 </h4>
+            <div className="csuImage-section">
+              <div className="csuImage-section-border">
+                <img className="csuImage" src={csu}></img>
               </div>
             </div>
-          </div>
-          <div className="csuImage-section">
-            <div className="csuImage-section-border">
-              <img className="csuImage" src={csu}></img>
+            <div className="csuLocation-card-lowerContent">
+              <div className="csuLocation-card-lowerContent-text">
+                <p>
+                  Established in 1870, Colorado State University (CSU) is a
+                  public research university in Fort Collins, Colorado, known
+                  for its diverse academic programs, strong research focus, and
+                  commitment to sustainability.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="csuLocation-card-lowerContent">
-            <p>
-              Established in 1870, Colorado State University (CSU) is a public
-              research university in Fort Collins, Colorado, known for its
-              diverse academic programs, strong research focus, and commitment
-              to sustainability.
-            </p>
           </div>
         </div>
       </div>

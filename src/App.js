db0ee90e510e1d1085ccf092f47361dfb78d7a1f
matt andrus <mattrandrus@gmail.com>
2023-10-12 12:59:32 -0600

    updoots

--- src/App.js
+++ src/App.js
@@ -18,40 +18,7 @@ import MouseTrail from "./Components/MouseTrail";
 import Swiping from "./Components/Swiping";
 
 function App() {
-  let component;
-  let array = [4, 6, 34, 56, 59, 89, 6, 9];
-  // switch (window.location.pathname) {
-  //   switch (document.querySelector(component)) {
-  //   case "/":
-  //     component = <About />;
-  //     break;
-  //   case "/Contact":
-  //     component = <Contact />;
-  //     break;
-  //   case "/Skills":
-  //     component = <Skills />;
-  //     break;
-  //   case "/About":
-  //     component = <About />;
-  //     break;
-  //   case "/Experience":
-  //     component = <Experience />
-  //     break;
-  //   default:
-  //     component = App;
-  //   break;
-  // }
-
-  let banana = (inHere) => {
-    let p = 0;
-    for (let i = 0; i < inHere.length; i++) {
-      p += inHere[i];
-    }
-    return p;
-  };
-
-  let c = banana(array);
-
+  
   return (
     <main>
       {/* <Swiping /> */}
